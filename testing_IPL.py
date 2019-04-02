d3ec141a122409653e68ab6426c293eacd5fe7c7
Rohit Swami <rowhitswami1@gmail.com>
2019-04-02 23:56:36 +0530

    Add Current Score

--- testing_IPL.py
+++ testing_IPL.py
@@ -7,26 +7,50 @@ Created on Mon Apr  1 21:47:36 2019
 """
 
 import requests 
-from bs4 import BeautifulSoup 
-URL = "https://demo.entitysport.com/matches/kings-xi-punjab-vs-delhi-capitals-40293/"
+from bs4 import BeautifulSoup
+import time
+from plyer import notification
 
+prev_ball_status = 0
+
+def sys_notification(title, commentary, current_score):
+    notification.notify(
+        title=title,
+        message=commentary+" "+current_score,
+        timeout=30
+        )
 
 while True:
-    URL = "https://demo.entitysport.com/matches/kings-xi-punjab-vs-delhi-capitals-40293/"
+    URL = "https://demo.entitysport.com/matches/rajasthan-royals-vs-royal-challengers-bangalore-40294/"
     headers = headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-    html = requests.get(URL, headers=headers) 
+    html = requests.get(URL, headers=headers)
     soup = BeautifulSoup(html.content, 'html5lib')
+
+    # Getting the match title
+    title = soup.find('h1', attrs={'id': 'heading'}).text.split(',')[0]
         
     # Getting the update from last ball
-    last_ball = soup.find('div', attrs={'class': 'live-info4'})
-    last_ball = last_ball.span.text.strip()
-    if current_status != prev_status:
-        if last_ball == 'W':
-            last_ball_commmentary = soup.find('div', attrs={'class': 'comment-wicket'})
-            last_ball_commmentary = last_ball_commmentary.find('div', attrs={'class':'text'}).text.strip()
-            print(last_ball_commmentary)
+    last_ball = soup.find('div', attrs={'class': 'live-info4'}).span.text.strip()
+
+    # Getting the current score
+    current_score = soup.find('div', attrs={'class': 'teamaScore'}).text.strip()
+
+    # Current ball of the over
+    current_ball_status = soup.find('div', attrs={'class': 'ovb'}).text.strip()
+    if current_ball_status != prev_ball_status:
+        if (last_ball == 'W'):
+            last_wicket_commmentary = soup.find('div', attrs={'class': 'comment-wicket'})
+            last_wicket_commmentary = last_wicket_commmentary.find('div', attrs={'class':'text'}).text.strip()
+            print("Last Wicket Commentary: "+ last_wicket_commmentary + " " + current_score)
+            sys_notification(title, last_wicket_commmentary, current_score)
+            time.sleep(60)
+            continue
         elif (last_ball == '4') or (last_ball == '6'):
-            last_ball_commmentary = soup.find('div', attrs={'class': 'comment-ball'})
-            last_ball_commmentary = last_ball_commmentary.find('div', attrs={'class':'text'}).text.strip()
-            print("Last Ball Commentary: ", last_ball_commmentary)
-        prev_status = current_status
+            last_hit_commmentary = soup.find('div', attrs={'class': 'comment-ball'})
+            last_hit_commmentary = last_hit_commmentary.find('div', attrs={'class':'text'}).text.strip()
+            print("Last Hit Commentary: "+ last_hit_commmentary + " " + current_score)
+            sys_notification(title, last_hit_commmentary, current_score)
+        elif current_ball_status == '19.6':
+            current_score = soup.find('div', attrs={'class': 'teamaScore'}).text.strip()
+        print(current_score)
+        prev_ball_status = current_ball_status
